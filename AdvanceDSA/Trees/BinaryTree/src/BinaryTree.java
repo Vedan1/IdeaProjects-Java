797fb033a222ac198fbe1faa9a531decc9efe1d8
vedanshii <vedanshi2718@gmail.com>
2023-11-01 19:58:44 +0530

    Binary Tree | Transversal Added.

---
+++ AdvanceDSA/Trees/BinaryTree/src/BinaryTree.java
@@ -0,0 +1,66 @@
+
+public class BinaryTree {
+    static Node root;
+    static Node left = null, right = null;
+
+
+    public static void insert(int data) {
+        Node n = new Node();
+        n.data = data;
+        n.left = null;
+        n.right = null;
+        if (root == null) {
+            root = n;
+            System.out.println("\t\t"+root.data+"\t\t-root");
+            System.out.println();
+        } else insert(root, n);
+
+
+    }
+
+    public static void insert(Node n, Node Nnode) {
+        if (Nnode.data < n.data) {
+            if (n.left == null) {
+                n.left = Nnode;
+                System.out.println(n.data+ "\t-left");
+            } else insert(n.left, Nnode);
+        } else {
+            if (n.right == null) {
+                n.right = Nnode;
+                System.out.println("\t-right "+n.data);
+            } else insert(n.right, Nnode);
+
+        }
+
+        System.out.println();
+    }
+    public static void inOrderTransversal(Node n){
+        if(n==null){
+            return;
+        }
+        inOrderTransversal(n.left);
+        System.out.print(n.data+" ");
+        inOrderTransversal(n.right);
+
+    }
+
+       public static void preOrderTransversal(Node n) {
+           if (n == null) {
+               return;
+           }
+           System.out.print(n.data + " ");
+           preOrderTransversal(n.left);
+           preOrderTransversal(n.right);
+
+       }
+        public static void postOrderTransversal(Node n) {
+           if (n == null) {
+               return;
+           }
+           postOrderTransversal(n.left);
+           postOrderTransversal(n.right);
+           System.out.print(n.data + " ");
+
+       }
+
+}
